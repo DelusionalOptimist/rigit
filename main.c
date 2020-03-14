141e1d3f0072341dc06e57ef2231add99347215c
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2020-03-15 02:29:06 +0530

    cli now uses argp

--- main.c
+++ main.c
@@ -1,86 +1,69 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <argp.h>
+
+const char *argp_program_version = "rigit v1.0";
+const char *argp_program_bug_address = "<https://github.com/DelusionalOptimist/rigit>";
+
+static char doc[] = "Rigit is a version control system.";
+
+static char args_doc[] = "ARG1 ARG2";
+
+static struct argp_option options[] = {
+  //{"add",          'a', 0,      0,  "Add file contents to the index" },
+  //{"checkout",     'c', 0,      0,  "Apply changes introduced by sone existing commits" },
+  //{"commit",       'C', 0,      0,  "Give an object a human readable name" },
+  //{"hash-object",  'h', 0,      0,  "" },
+  {"init",         'i', 0,      0,  "Create an empty git repository" },
+  //{"log",          'l', 0,      0,  "" },
+  //{"merge",        'm', 0,      0,  "" },
+  //{"rebase",       'r', 0,      0,  "" },
+  //{"rm",           'R', 0,      0,  "" },
+  //{"tag",          't', 0,      0,  "" },
+  { 0 }
+};
+
+struct arguments
 {
-    char operation[][15]={"add","checkout","commit","hash-object","init","log","merge","rebase","rm","tag"};
-    int choice;
-    int i=0;
-    if (argc<=2)
+  char *args[2];
+};
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = state->input;
+
+  switch (key)
+  {
+    case 'i': 
     {
-        while(1)
-        {
-            if ((strcmp(argv[1],operation[i]))==0
-            )
-            {
-                choice=i;
-                break;
-            }
-            ++i;
-        }
+      printf("init does not work yet\n");
+      break;
     }
-    else
+    case ARGP_KEY_ARG:
     {
-        //help function
-        exit(0);
+      if (state->arg_num >= 1)
+      {
+        argp_usage (state);
+      }
+      arguments->args[state->arg_num] = arg;
+      break;
     }
-    
-    switch (choice)
+    default:
     {
-        case 0:
-            {
-                //call add function
-                break;
-            }
-        case 1:
-            {
-                //call checkout function
-                break;
-            }
-        case 2:
-            {
-                //call commit function
-                break;
-            }
-        case 3:
-            {
-                //hash-object function
-                break;
-            }
-        case 4:
-            {
-                //init function
-                break;
-            }
-        case 5:
-            {
-                //log function
-                break;
-            }
-        case 6:
-            {
-                //merge
-                break;
-            }
-        case 7:
-            {
-                //rebase
-                break;
-            }
-        case 8:
-            {
-                //rm
-                break;
-            }
-        case 9:
-            {
-                //tag
-                break;
-            }
-        default:
-            {
-                //call help function
-            }
+      return ARGP_ERR_UNKNOWN;
     }
-    return 0;
+  }
+
+  return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+int main (int argc, char **argv)
+{
+  struct arguments arguments;
+
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  exit (0);
 }
